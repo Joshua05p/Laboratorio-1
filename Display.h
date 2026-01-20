9ca254ab131c4d961b6801e939162bbc982a0d72
Joshua05p <per23395@uvg.edu.gt>
2026-01-19 21:58:41 -0600

    Create Display.h

---
+++ Display.h
@@ -0,0 +1,17 @@
+/*
+ * Display.h
+ *
+ * Created: 19/01/2026 21:45:47
+ *  Author: perez
+ */ 
+
+
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+#include <avr/io.h>
+
+void f_display(int valor);
+
+
+#endif /* DISPLAY_H_ */
